45a2ddbfb63ed8d80dec2948bb0cdcc2aa000ae9
alordash <alordash1@gmail.com>
2026-09-25 15:47:03 +0400

    disable rsubstitute tests

--- rsubstitute_core/src/fn_parameters/dyn_arg_refs_tuple.rs
+++ rsubstitute_core/src/fn_parameters/dyn_arg_refs_tuple.rs
@@ -1,11 +1,11 @@
 use crate::fn_parameters::*;
 
-#[cfg_attr(test, rsubstitute::mock)]
+// #[cfg_attr(test, rsubstitute::mock)]
 pub struct DynArgRefsTuple<'rs> {
     inner: Option<Box<dyn IArgRefsTuple<'rs> + 'rs>>,
 }
 
-#[cfg_attr(test, rsubstitute::mock(base))]
+// #[cfg_attr(test, rsubstitute::mock(base))]
 impl<'rs> DynArgRefsTuple<'rs> {
     pub(crate) fn zero_size() -> Self {
         Self { inner: None }
@@ -34,45 +34,45 @@ impl<'rs> DynArgRefsTuple<'rs> {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    #![allow(non_snake_case)]
-
-    use super::*;
-    use rsubstitute::Mockable;
-
-    #[test]
-    fn from_raw_Ok() {
-        // Arrange
-        let arg_refs_tuple = (1, 2, 3);
-        let boxed: Box<dyn IArgRefsTuple> = Box::new(arg_refs_tuple);
-        let ptr = Box::leak(boxed) as *mut _;
-
-        // Act
-        let result = DynArgRefsTuple::from_raw(ptr);
-
-        // Assert
-        let actual_ptr = result.inner.expect("from_raw -> must be Some").as_ref() as *const _;
-        assert_eq!(actual_ptr, ptr);
-    }
-
-    #[test]
-    fn downcast_into_Ok() {
-        // Arrange
-        type ArgRefsTupleType = (i32, i32, i32);
-        let arg_refs_tuple: ArgRefsTupleType = (1, 2, 3);
-        let boxed: Box<dyn IArgRefsTuple> = Box::new(arg_refs_tuple);
-        let ptr = Box::leak(boxed) as *mut _;
-        let mut dyn_arg_refs_tuple = DynArgRefsTuple::from_raw(ptr);
-        dyn_arg_refs_tuple
-            .setup()
-            .downcast_into::<ArgRefsTupleType>()
-            .call_base();
-
-        // Act
-        let result: ArgRefsTupleType = dyn_arg_refs_tuple.downcast_into();
-
-        // Assert
-        assert_eq!(result, arg_refs_tuple);
-    }
-}
+// #[cfg(test)]
+// mod tests {
+//     #![allow(non_snake_case)]
+// 
+//     use super::*;
+//     use rsubstitute::Mockable;
+// 
+//     #[test]
+//     fn from_raw_Ok() {
+//         // Arrange
+//         let arg_refs_tuple = (1, 2, 3);
+//         let boxed: Box<dyn IArgRefsTuple> = Box::new(arg_refs_tuple);
+//         let ptr = Box::leak(boxed) as *mut _;
+// 
+//         // Act
+//         let result = DynArgRefsTuple::from_raw(ptr);
+// 
+//         // Assert
+//         let actual_ptr = result.inner.expect("from_raw -> must be Some").as_ref() as *const _;
+//         assert_eq!(actual_ptr, ptr);
+//     }
+// 
+//     #[test]
+//     fn downcast_into_Ok() {
+//         // Arrange
+//         type ArgRefsTupleType = (i32, i32, i32);
+//         let arg_refs_tuple: ArgRefsTupleType = (1, 2, 3);
+//         let boxed: Box<dyn IArgRefsTuple> = Box::new(arg_refs_tuple);
+//         let ptr = Box::leak(boxed) as *mut _;
+//         let mut dyn_arg_refs_tuple = DynArgRefsTuple::from_raw(ptr);
+//         dyn_arg_refs_tuple
+//             .setup()
+//             .downcast_into::<ArgRefsTupleType>()
+//             .call_base();
+// 
+//         // Act
+//         let result: ArgRefsTupleType = dyn_arg_refs_tuple.downcast_into();
+// 
+//         // Assert
+//         assert_eq!(result, arg_refs_tuple);
+//     }
+// }
--- rsubstitute_core/src/fn_parameters/dyn_call.rs
+++ rsubstitute_core/src/fn_parameters/dyn_call.rs
@@ -49,160 +49,160 @@ impl<'rs> DynCall<'rs> {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    #![allow(non_snake_case)]
-
-    use super::*;
-    use crate::fn_parameters::tests::*;
-    use rsubstitute::{AsTimes, Mockable};
-
-    #[test]
-    fn new_Ok() {
-        // Arrange
-        let mut call_mock = CallMock::new();
-        call_mock
-            .setup()
-            .as_ICall()
-            .get_arg_infos()
-            .returns(Vec::new());
-
-        // Act
-        let result = DynCall::new(call_mock);
-
-        // Assert
-        let arg_infos = result.inner.get_arg_infos();
-        assert!(arg_infos.is_empty());
-    }
-
-    #[test]
-    fn downcast_ref_Ok() {
-        // Arrange
-        let mut call_mock = CallMock::new();
-        call_mock.id = 123;
-        let dyn_call = DynCall::new(call_mock.clone());
-
-        // Act
-        let result: &CallMock = dyn_call.downcast_ref();
-
-        // Assert
-        assert_eq!(result.id, call_mock.id);
-    }
-
-    #[test]
-    fn ICall_get_arg_infos_ForwardsToInner() {
-        // Arrange
-        let mut call_mock = CallMock::new();
-        let arg_infos = vec![
-            ArgInfo::new("quo 1", "vadis 1", "veridis 1".to_owned()),
-            ArgInfo::new("quo 2", "vadis 2", "veridis 2".to_owned()),
-        ];
-        call_mock
-            .setup()
-            .as_ICall()
-            .get_arg_infos()
-            .returns(arg_infos.clone());
-
-        let dyn_call = DynCall::new(call_mock.clone());
-
-        // Act
-        let result = dyn_call.get_arg_infos();
-
-        // Assert
-        assert_eq!(result, arg_infos);
-        call_mock
-            .received()
-            .as_ICall()
-            .get_arg_infos(1.time())
-            .no_other_calls();
-    }
-
-    #[test]
-    fn ICall_get_ptr_to_boxed_tuple_of_refs_ForwardsToInner() {
-        // Arrange
-        let mut call_mock = CallMock::new();
-        let ptr = 123usize as *mut ();
-        call_mock
-            .setup()
-            .as_ICall()
-            .get_ptr_to_boxed_tuple_of_refs()
-            .returns(ptr);
-
-        let dyn_call = DynCall::new(call_mock.clone());
-
-        // Act
-        let result = dyn_call.get_ptr_to_boxed_tuple_of_refs();
-
-        // Assert
-        assert_eq!(result, ptr);
-        call_mock
-            .received()
-            .as_ICall()
-            .get_ptr_to_boxed_tuple_of_refs(1.time())
-            .no_other_calls();
-    }
-
-    #[test]
-    fn IGenericsInfoProvider_get_generic_parameter_infos_ForwardsToInner() {
-        // Arrange
-        let generic_parameter_infos = vec![GenericParameterInfo::Type(GenericTypeInfo {
-            name: "quo",
-            type_name: "vadis",
-        })];
-        let mut call_mock = CallMock::new();
-        call_mock
-            .setup()
-            .as_IGenericsInfoProvider()
-            .get_generic_parameter_infos()
-            .returns(generic_parameter_infos.clone());
-        let dyn_call = DynCall::new(call_mock.clone());
-
-        // Act
-        let result = dyn_call.get_generic_parameter_infos();
-
-        // Assert
-        assert_eq!(result, generic_parameter_infos);
-
-        call_mock
-            .received()
-            .as_IGenericsInfoProvider()
-            .get_generic_parameter_infos(1.time())
-            .no_other_calls();
-    }
-
-    #[test]
-    fn IGenericsInfoProvider_hash_generics_type_ids_ForwardsToInner() {
-        // Arrange
-        let mut generics_hasher = GenericsHasher::new();
-        let mut call_mock = CallMock::new();
-        let dyn_call = DynCall::new(call_mock.clone());
-
-        // Act
-        dyn_call.hash_generics_type_ids(&mut generics_hasher);
-
-        // Assert
-        call_mock
-            .received()
-            .as_IGenericsInfoProvider()
-            .hash_generics_type_ids(rsubstitute::Arg::ref_eq(&mut generics_hasher), 1.time())
-            .no_other_calls();
-    }
-
-    #[test]
-    fn IGenericsInfoProvider_hash_const_values_ids_ForwardsToInner() {
-        // Arrange
-        let mut generics_hasher = GenericsHasher::new();
-        let mut call_mock = CallMock::new();
-        let dyn_call = DynCall::new(call_mock.clone());
-
-        // Act
-        dyn_call.hash_const_values(&mut generics_hasher);
-
-        // Assert
-        call_mock
-            .received()
-            .as_IGenericsInfoProvider()
-            .hash_const_values(rsubstitute::Arg::ref_eq(&mut generics_hasher), 1.time())
-            .no_other_calls();
-    }
-}
+// #[cfg(test)]
+// mod tests {
+//     #![allow(non_snake_case)]
+// 
+//     use super::*;
+//     use crate::fn_parameters::tests::*;
+//     use rsubstitute::{AsTimes, Mockable};
+// 
+//     #[test]
+//     fn new_Ok() {
+//         // Arrange
+//         let mut call_mock = CallMock::new();
+//         call_mock
+//             .setup()
+//             .as_ICall()
+//             .get_arg_infos()
+//             .returns(Vec::new());
+// 
+//         // Act
+//         let result = DynCall::new(call_mock);
+// 
+//         // Assert
+//         let arg_infos = result.inner.get_arg_infos();
+//         assert!(arg_infos.is_empty());
+//     }
+// 
+//     #[test]
+//     fn downcast_ref_Ok() {
+//         // Arrange
+//         let mut call_mock = CallMock::new();
+//         call_mock.id = 123;
+//         let dyn_call = DynCall::new(call_mock.clone());
+// 
+//         // Act
+//         let result: &CallMock = dyn_call.downcast_ref();
+// 
+//         // Assert
+//         assert_eq!(result.id, call_mock.id);
+//     }
+// 
+//     #[test]
+//     fn ICall_get_arg_infos_ForwardsToInner() {
+//         // Arrange
+//         let mut call_mock = CallMock::new();
+//         let arg_infos = vec![
+//             ArgInfo::new("quo 1", "vadis 1", "veridis 1".to_owned()),
+//             ArgInfo::new("quo 2", "vadis 2", "veridis 2".to_owned()),
+//         ];
+//         call_mock
+//             .setup()
+//             .as_ICall()
+//             .get_arg_infos()
+//             .returns(arg_infos.clone());
+// 
+//         let dyn_call = DynCall::new(call_mock.clone());
+// 
+//         // Act
+//         let result = dyn_call.get_arg_infos();
+// 
+//         // Assert
+//         assert_eq!(result, arg_infos);
+//         call_mock
+//             .received()
+//             .as_ICall()
+//             .get_arg_infos(1.time())
+//             .no_other_calls();
+//     }
+// 
+//     #[test]
+//     fn ICall_get_ptr_to_boxed_tuple_of_refs_ForwardsToInner() {
+//         // Arrange
+//         let mut call_mock = CallMock::new();
+//         let ptr = 123usize as *mut ();
+//         call_mock
+//             .setup()
+//             .as_ICall()
+//             .get_ptr_to_boxed_tuple_of_refs()
+//             .returns(ptr);
+// 
+//         let dyn_call = DynCall::new(call_mock.clone());
+// 
+//         // Act
+//         let result = dyn_call.get_ptr_to_boxed_tuple_of_refs();
+// 
+//         // Assert
+//         assert_eq!(result, ptr);
+//         call_mock
+//             .received()
+//             .as_ICall()
+//             .get_ptr_to_boxed_tuple_of_refs(1.time())
+//             .no_other_calls();
+//     }
+// 
+//     #[test]
+//     fn IGenericsInfoProvider_get_generic_parameter_infos_ForwardsToInner() {
+//         // Arrange
+//         let generic_parameter_infos = vec![GenericParameterInfo::Type(GenericTypeInfo {
+//             name: "quo",
+//             type_name: "vadis",
+//         })];
+//         let mut call_mock = CallMock::new();
+//         call_mock
+//             .setup()
+//             .as_IGenericsInfoProvider()
+//             .get_generic_parameter_infos()
+//             .returns(generic_parameter_infos.clone());
+//         let dyn_call = DynCall::new(call_mock.clone());
+// 
+//         // Act
+//         let result = dyn_call.get_generic_parameter_infos();
+// 
+//         // Assert
+//         assert_eq!(result, generic_parameter_infos);
+// 
+//         call_mock
+//             .received()
+//             .as_IGenericsInfoProvider()
+//             .get_generic_parameter_infos(1.time())
+//             .no_other_calls();
+//     }
+// 
+//     #[test]
+//     fn IGenericsInfoProvider_hash_generics_type_ids_ForwardsToInner() {
+//         // Arrange
+//         let mut generics_hasher = GenericsHasher::new();
+//         let mut call_mock = CallMock::new();
+//         let dyn_call = DynCall::new(call_mock.clone());
+// 
+//         // Act
+//         dyn_call.hash_generics_type_ids(&mut generics_hasher);
+// 
+//         // Assert
+//         call_mock
+//             .received()
+//             .as_IGenericsInfoProvider()
+//             .hash_generics_type_ids(rsubstitute::Arg::ref_eq(&mut generics_hasher), 1.time())
+//             .no_other_calls();
+//     }
+// 
+//     #[test]
+//     fn IGenericsInfoProvider_hash_const_values_ids_ForwardsToInner() {
+//         // Arrange
+//         let mut generics_hasher = GenericsHasher::new();
+//         let mut call_mock = CallMock::new();
+//         let dyn_call = DynCall::new(call_mock.clone());
+// 
+//         // Act
+//         dyn_call.hash_const_values(&mut generics_hasher);
+// 
+//         // Assert
+//         call_mock
+//             .received()
+//             .as_IGenericsInfoProvider()
+//             .hash_const_values(rsubstitute::Arg::ref_eq(&mut generics_hasher), 1.time())
+//             .no_other_calls();
+//     }
+// }
